1ef1019793a926e2250bccf7eb5f71bf79cacbc5
adba98 <oscar.adba@gmail.com>
2021-07-09 17:20:36 -0500

    Agregar diseño
    Se agrega diseño de componentes y sus respectivas rutas

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 
+interface itemData {
+  titulo: string;
+  icon: string;
+  url: string;
+  cursor?: boolean;
+}
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent implements OnInit {
+  items: itemData[] = [
+    {
+      titulo: 'Dashboard',
+      icon: 'fa-tachometer-alt',
+      url: '',
+    },
+    {
+      titulo: 'Ingresos y Egresos',
+      icon: 'fa-clipboard-list',
+      url: 'ingreso-egreso',
+    },
+    {
+      titulo: 'Detalle',
+      icon: 'fa-table',
+      url: 'detalle',
+    },
+    {
+      titulo: 'Cerrar sesión',
+      icon: 'fa-sign-out-alt',
+      url: 'login',
+      cursor: true,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
