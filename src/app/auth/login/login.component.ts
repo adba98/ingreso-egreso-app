3dadcc9c3eb2168552d012866fef0502f4e73ad2
adba98 <oscar.adba@gmail.com>
2021-07-09 22:01:36 -0500

    Agregar Login con Firebase

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
   }
 
+  public loginUsuario(): void {
+    if (this.loginForm.invalid) return;
+    const { email, password } = this.loginForm.value;
+    this.authService
+      .loginUsuario(email, password)
+      .then((credenciales) => {
+        console.log(credenciales);
+        this.router.navigate(['/']);
+      })
+      .catch((err) => console.error(err));
+  }
 }
