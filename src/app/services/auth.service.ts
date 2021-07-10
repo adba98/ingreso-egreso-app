4230fa18a615d6c63dfaa0470b2ba61e7ceac5d6
adba98 <oscar.adba@gmail.com>
2021-07-10 11:12:11 -0500

    Guardar usuario en FireStore

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,16 +1,24 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import { Usuario } from '../models/usuario.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private auth: AngularFireAuth) {}
+  constructor(
+    private auth: AngularFireAuth,
+    private firestore: AngularFirestore
+  ) {}
 
-  crearUsuario(nombre: string, email: string, password: string) {
-    return this.auth.createUserWithEmailAndPassword(email, password);
+  public async crearUsuario(nombre: string, email: string, password: string) {
+    const { user } = await this.auth
+      .createUserWithEmailAndPassword(email, password);
+    const usuraio = new Usuario(user!.uid, nombre, email);
+    return await this.firestore.doc(`${user!.uid}/usuario`).set({ ...usuraio });
   }
 
   loginUsuario(email: string, password: string) {
