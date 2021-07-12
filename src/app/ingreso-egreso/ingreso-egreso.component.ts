17be9a83d66e3e0b62b4992b25d85f9cec417c8a
adba98 <oscar.adba@gmail.com>
2021-07-12 00:07:29 -0500

    Agregar formulario ingreso y egreso

--- src/app/ingreso-egreso/ingreso-egreso.component.ts
+++ src/app/ingreso-egreso/ingreso-egreso.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-ingreso-egreso',
   templateUrl: './ingreso-egreso.component.html',
-  styleUrls: ['./ingreso-egreso.component.css']
+  styleUrls: ['./ingreso-egreso.component.css'],
 })
 export class IngresoEgresoComponent implements OnInit {
+  ingresoEgresoForm!: FormGroup;
+  tipo: string = 'ingreso';
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.ingresoEgresoForm = this.fb.group({
+      desc: ['', Validators.required],
+      monto: ['', Validators.required],
+    });
   }
 
+  public guardar(): void {
+    console.log(this.ingresoEgresoForm.value);
+  }
 }
